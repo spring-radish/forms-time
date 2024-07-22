5d5c7635fabfcf114b0c632aa58fb34cf3d5ae66
spring <mail@sprig.site>
2024-07-22 15:43:21 -0500

    organize

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
+/**
+ * Imports
+ */
 import { dispatchUrl } from "./api.js";
-import { intoYear } from "./year.js";
 import { renderDay } from "./blocks.js";
 import { blocksToYear } from "./timestructures.js";
-import cachedResponse from "./cachedApi.json" assert { type: "json" };
+// import cachedResponse from "./cachedApi.json" assert { type: "json" };
 
+/**
+ * Elements
+ */
 const channelPicker = document.getElementById("channel-picker");
 const channelUrl = document.getElementById("channel-url");
 const channelGo = document.getElementById("channel-go");
@@ -12,65 +17,95 @@ const calendarList = document.getElementById("calendar-list");
 const zoomSlider = document.getElementById("zoom-slider");
 const yearsLegend = document.getElementById("years-legend");
 
-let BIGCAL = null;
+/**
+ * State
+ */
+let BIGYEAR = undefined;
 
+
+
+/**
+ * Overall Plan
+ */
 function init() {
 	channelPicker.addEventListener("submit", clickDo);
 	zoomSlider.addEventListener("input", scaleGrid);
-	// const message = await addDays(null)
-	// status.innerText = message
-}
-
-function todaysId() {
-	const today = new Date();
-	const month = today.getMonth() + 1;
-	const day = today.getDate();
-	return `li[id="${month}-${day}"] {border: 3px double inherit}`;
-}
-
-async function addDays(url) {
-	const { message, blocks } = {
-		message: "ready to go",
-		blocks: cachedResponse.contents,
-	};
-	// const {message, blocks} = url ? await dispatchUrl(url)
-	//     : {message: 'ready to go', blocks: null}
-
-	console.log("message", message);
-	console.log(blocks);
-
-	const newDays = blocksToYear(blocks);
-
-	for (const [date, blocks] of newDays) {
-		const li = document.getElementById(date);
-		li.style.background = "pink";
-	}
-
-	return message;
-}
-
-function makeColor(year) {}
-
-async function clickDo(e) {
-	e.preventDefault();
-	channelUrl.setAttribute("disabled", "");
-	channelGo.setAttribute("disabled", "");
-
-	const url = channelUrl.value;
-
-	status.innerText = `getting ${url}`;
-
-	const message = await addDays(url);
-
-	status.innerText = message;
-
-	channelUrl.removeAttribute("disabled");
-	channelGo.removeAttribute("disabled");
-}
-
-function scaleGrid(e) {
-	const zoomLevel = e.target.value;
-	calendarList.style = `--zoom: ${zoomLevel}rem`;
+	highlightToday();
+	status.innerText = "ready to go";
 }
 
 init();
+
+
+
+/**
+ * Behind the Scenes
+ */
+async function clickDo(e) {
+    // Hold the phone
+    e.preventDefault();
+
+    // Close the portal
+    channelUrl.setAttribute("disabled", "");
+    channelGo.setAttribute("disabled", "");
+
+    // Repeat the question
+    const url = channelUrl.value;
+    status.innerText = `getting ${url}`;
+
+    // Seek the answer
+    const {message, blocks} = await dispatchUrl(url);
+
+    // Amend the tome
+    BIGYEAR = blocksToYear(blocks);
+    addDays()
+    updateYears()
+    status.innerText = message;
+
+    // Reopen the portal
+    channelUrl.removeAttribute("disabled");
+    channelGo.removeAttribute("disabled");
+}
+
+/**
+ * Pieces of the Puzzle
+ */
+function addDays() {
+    for (const [date, blocks] of BIGYEAR.days) {
+        const li = document.getElementById(date);
+        if (!li) {
+            console.log(`Skipping ${blocks.length} block(s) with date ${date}.`);
+            continue;
+        }
+        li.innerHTML = renderDay(blocks, date);
+    }
+}
+
+function updateYears() {
+    yearsLegend.innerHTML = [...BIGYEAR.info.yearsRepresented]
+        .sort()
+        .map((y) => `<li class='year-${y}'>${y}</li>`)
+        .join(" ");
+}
+
+function scaleGrid(e) {
+    const zoomLevel = e.target.value;
+    calendarList.style = `--zoom: ${zoomLevel}rem`;
+}
+
+function highlightToday() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    const id = `${month}-${day}`;
+
+    document.getElementById(id).classList.add('today');
+}
+
+
+
+/**
+ * Unimplemented function to programatically
+ * generate a color scale for yearsLegend
+ */
+function makeColor(year) {}
