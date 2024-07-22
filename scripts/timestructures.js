54de01e1393dc6bda7ee79c7cb71b4b721ad0b2b
spring <mail@sprig.site>
2024-07-22 12:41:49 -0500

    get date detection tests working

--- scripts/timestructures.js
+++ scripts/timestructures.js
@@ -1,30 +1,80 @@
 export function blocksToYear(blocks) {
-    const year = new Map()
+	const year = new Map();
 
-    for (const block of blocks) {
-        const date = detectDate(block)
-        const oldday = year.get(date) || []
-        const newday = oldday.concat(block)
-        year.set(date, newday)
-    }
+	for (const block of blocks) {
+		const clues = [
+			block.title,
+			block.content,
+			block.connected_at,
+			block.created_at,
+		];
+		const date = detectDate(clues);
+		const yearday = year.get(date) || [];
+		const newday = oldday.concat(block); // use .push if performance needs it
+		year.set(date, newday);
+	}
 
-    return [...year]
+	return [...year];
 }
 
-export function detectDate(block) {
-    const formats = new RegExp(/(\d{1,2}\W\d{1,2}\W\d{2,4})|(\d{2,4}\W?\d{2}\W?\d{2})|(\d{1,2}\D{3,12}\d{2,4})|([A-Za-z,]{3,12}\d{1,2}\W{0,2}\d{4})/g)
-    const textual = new String(block.title).concat(block.content)
-    const matches = textual.match(formats) || []
-    // console.log(matches)
+export function detectDate(clues) {
+	return clues.flatMap(tryDetectors).find(truthy);
+}
 
-    const fallback = block.connected_at || block.created_at
-    const options = matches.concat(fallback)
+function tryDetectors(clue) {
+	return detectors.map((detector) => detector(String(clue)));
+}
 
-    const validDate = options.find(m => !isNaN(Date.parse(m)))
-    // console.log(options)
+const detectors = [
+	(clue) => {
+		// 2033.01.22 or 20330122
+		const match = clue.match(/\d{4}\W?(\d{2})\W?(\d{2})/);
+		if (!match) return null;
+		const month = trimLeadingZero(match[1]);
+		const day = trimLeadingZero(match[2]);
+		return `${month}-${day}`;
+	},
+	(clue) => {
+		// 1-22-33 or 01-22-2033
+		const match = clue.match(/(\d{1,2})\W(\d{1,2})\W\d{2,4}/);
+		if (!match) return null;
+		const month = trimLeadingZero(match[1]);
+		const day = trimLeadingZero(match[2]);
+		return `${month}-${day}`;
+	},
+	(clue) => {
+		// 33.01.22 or 330122
+		const match = clue.match(/\d{2}\W?(\d{2})\W?(\d{2})/);
+		if (!match) return null;
+		const month = trimLeadingZero(match[1]);
+		const day = trimLeadingZero(match[2]);
+		return `${month}-${day}`;
+	},
+	(clue) => {
+		// January 22, 2033
+		const match = clue.match(/([A-Za-z]{3,12}),? ?(\d{1,2})\W{0,2}\d{4}/);
+		if (!match) return null;
+		const date = new Date(match[0]);
+		const month = date.getMonth() + 1;
+		const day = trimLeadingZero(match[2]);
+		return `${month}-${day}`;
+	},
+	(clue) => {
+		// 22 January 2033
+		const match = clue.match(/(\d{1,2}) ?([A-Za-z]{3,12}),? ?\d{2,4}/);
+		if (!match) return null;
+		const date = new Date(match[0]);
+		const month = date.getMonth() + 1;
+		const day = trimLeadingZero(match[1]);
+		return `${month}-${day}`;
+	},
+];
 
-    const date = new Date(validDate)
-    const month = date.getMonth() + 1
-    const day = date.getDate()
-    return `${month}-${day}`
+function trimLeadingZero(str) {
+	if (str.startsWith("0")) return str.slice(1);
+	return str;
+}
+
+function truthy(x) {
+	return Boolean(x);
 }
--- tests/time.test.js
+++ tests/time.test.js
@@ -2,36 +2,56 @@ import { detectDate } from "../scripts/timestructures"
 import { expect, test } from "vitest"
 
 test('date in title: M/D/YY', () => {
-    const block = {title: '1/1/24'}
-    expect(detectDate(block)).toBe('1-1')
+    const block = ['1/2/24']
+    expect(detectDate(block)).toBe('1-2')
 })
 
 test('date in title: M/D/YYYY', () => {
-    const block = {title: '1/2/2024'}
+    const block = ['1/2/2024']
     expect(detectDate(block)).toBe('1-2')
 })
 
 test('date in title: MM/DD/YY', () => {
-    const block = {title: '02/11/24'}
+    const block = ['02/11/24']
     expect(detectDate(block)).toBe('2-11')
 })
 
 test('date in title: YYYY-MM-DD', () => {
-    const block = {title: '2023-11-11'}
-    expect(detectDate(block)).toBe('11-11')
+    const block = ['2023-11-01']
+    expect(detectDate(block)).toBe('11-1')
 })
 
 test('date in title: YYYYMMDD', () => {
-    const block = {title: '20221111'}
-    expect(detectDate(block)).toBe('11-11')
+    const block = ['20221101']
+    expect(detectDate(block)).toBe('11-1')
 })
 
 test('date in text: DD mmmmm, YYYY', () => {
-    const block = {title: undefined, content: 'Friend #019 23 May, 2024 Jennifer'}
+    const block = [undefined, 'Friend #019 23 May, 2024 Jennifer']
     expect(detectDate(block)).toBe('5-23')
 })
 
 test('date in text: mmmmm DDth, YYYY', () => {
-    const block = {title: undefined, content: 'Friend #018 April 30, 2024 Bruno'}
+    const block = [undefined, 'Friend #018 April 30, 2024 Bruno']
     expect(detectDate(block)).toBe('4-30')
+})
+
+test('dates not to recognize', () => {
+    const block = ['David Wojnarowicz - Cross Country 1: February-March 1989 (Part 1)', '2021-08-31T08:56:59.785Z']
+    expect(detectDate(block)).toBe('8-31')
+})
+
+test('numbers not to recognize', () => {
+    const block = ['1 to 40 side by side', '2020-06-29T16:31:38.846Z']
+    expect(detectDate(block)).toBe('6-29')
+})
+
+test('what about ranges?', () => {
+    const block = ['4/6/23–4/8/23']
+    expect(detectDate(block)).toBe('4-6')
+})
+
+test('abbreviated month', () => {
+    const block = ['31Jan2024']
+    expect(detectDate(block)).toBe('1-31')
 })
